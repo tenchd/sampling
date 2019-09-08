a03c14577dfbe708e22ea954c72c49ea69a50aab
tenchd <dmt213@gmail.com>
2019-09-08 19:11:55 -0400

    core bug testing

--- f_0sketcher.py
+++ f_0sketcher.py
@@ -34,23 +34,44 @@ class HashFn():
             return True
         return False
 
-def T_checker(T, stream, eps, delta):
+def T_checker(T, stream, eps, delta, k_wt=1, display=False):
     #k = O(eps^(-2) log(delta^-1))
-    k = int(np.power(eps, -2) * np.log(np.power(delta,-1)))
-    print(k/math.e)
+    k = int(k_wt * np.power(eps, -2) * np.log(np.power(delta,-1)))
     hashes = [HashFn() for i in range(k)]
     for index, element in enumerate(stream):
         for h in hashes:
             h.decider(index, element, T)
     nonzeroz = len(list(filter(lambda h: h.total != 0, hashes)))
-    print(nonzeroz)
+    if display:
+        print(k/math.e)
+        print(nonzeroz)
+        if nonzeroz < k/math.e:
+            print("F_0 < (1-{})T = {} bro. You guessed too high".format(eps, (1-eps)*T))
+        else:
+            print("F_0 > (1+{})T  = {} superchief.  You guessed too low".format(eps, (1+eps)*T))
     if nonzeroz < k/math.e:
-        print("F_0 < (1-{})T = {} bro".format(eps, (1-eps)*T))
+        return True
     else:
-        print("F_0 > (1+{})T  = {} superchief".format(eps, (1+eps)*T))
-   # print([h.total for h in hashes])
+        return False
 
 if __name__ == '__main__':
-    stream = np.identity((100), dtype=int).reshape(10000,)
-    T_checker(200, stream, .2, .1)
+    F_0 = 100
+    stream = np.identity((F_0), dtype=int).reshape(F_0**2,)
+    T = 150
+    ks = [1, 1/2, 1/5, 1/10, 1/20]
+    if T > F_0:
+        too_high = True
+    else:
+        too_high = False
+
+    for k in ks:
+        mistakes = 0
+        print("testing F_0 = {}, T = {}, k factor = {}".format(F_0, T, k))
+        reps = 100
+        for i in range(reps):
+            print(".", end = '')
+            result = T_checker(T, stream, .2, .01, k_wt=k, display=False)
+            if result != too_high:
+                mistakes = mistakes + 1
+        print("{}/{} mistakes".format(mistakes, reps))
     
