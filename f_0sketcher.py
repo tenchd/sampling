24c5ee3d77b8ac62f6c6bb10b8c30f0806883ad6
tenchd <dmt213@gmail.com>
2019-09-09 14:37:07 -0400

    threshold bug fixed

--- f_0sketcher.py
+++ f_0sketcher.py
@@ -8,20 +8,21 @@ Created on Sun Sep  8 12:59:26 2019
 
 import numpy as np
 import xxhash
-from random import getrandbits, randint
+from random import getrandbits
 import math
 import struct
 
 class HashFn():
-    """wrapper class for xxhash objects"""
+    """Represents a random subset of [n].  Keeps running total of the value of
+    all stream updates whose indicies are in this random subset."""
     def __init__(self):
         seed = getrandbits(32)
         self.fn = xxhash.xxh32(seed = seed)
         self.total = 0
-        self.bins = set()
+        #self.bins = set()
     
     def hasher(self, num):
-        """give it a number (index for vector), it returns a random integer"""
+        """Input: an integer (index for vector).  Output: a random integer."""
         self.fn.reset()
         #self.fn.update(bytes([num]))
         byte_form = struct.pack(">I", num)
@@ -29,11 +30,14 @@ class HashFn():
         return self.fn.intdigest()
     
     def decider(self, index, value, T):
-        """give it a number and a T estimate. it'll return yes w/p 1/T"""
+        """Input: index and value of vector update and a T estimate (all 
+        integers).  Return yes w/p 1/T by hashing index.  If yes, add value to 
+        running total."""
         hashed = self.hasher(index)
-        if hashed <= 4294967296/T:
+        #2^32-1 = 4294967295 is max output value from hash function
+        if hashed <= 4294967295/T: 
             self.total = self.total + value
-            self.bins.add(index)
+            #self.bins.add(index)
             return True
         return False
 
@@ -45,9 +49,12 @@ def calc_k(eps, delta):
     result = term1*frac
     return result
 
-def T_checker(T, stream, eps, delta, k_wt=1, display=False):
-    k = int(k_wt * np.power(eps, -2) * np.log(np.power(delta,-1)))
-    #k = int(calc_k(eps, delta))
+def T_checker(T, stream, eps, delta, real_k = True, display=False):
+    if real_k:
+        k = int(calc_k(eps, delta))
+    else:
+        k = int(np.power(eps, -2) * np.log(np.power(delta,-1)))
+    
     hashes = [HashFn() for i in range(k)]
     for index, element in enumerate(stream):
         for h in hashes:
@@ -82,24 +89,23 @@ def T_checker(T, stream, eps, delta, k_wt=1, display=False):
 if __name__ == '__main__':
     #stream = np.identity(10, dtype=int).reshape(100,)
     #T_checker(12, stream, .2, .01, k_wt=1, display=True)
-    F_0 = 100
+    F_0 = 10
     stream = np.identity((F_0), dtype=int).reshape(F_0**2,)
-    T = 150
-    #ks = [1, 1/2, 1/5, 1/10, 1/20]
-    ks = [1]
+    T = 12
     if T > F_0:
         too_high = True
     else:
         too_high = False
-
-    for k in ks:
-        mistakes = 0
-        print("testing F_0 = {}, T = {}, k factor = {}".format(F_0, T, k))
-        reps = 100
-        for i in range(reps):
+    real_k = False
+    mistakes = 0
+    print("testing F_0 = {}, T = {}, real k? {}".format(F_0, T, real_k))
+    reps = 100
+    for i in range(reps):
+        result = T_checker(T, stream, .2, .01, display=False)
+        if result != too_high:
+            mistakes = mistakes + 1
+            print("!", end = '')
+        else:
             print(".", end = '')
-            result = T_checker(T, stream, .02, .01, k_wt=k, display=False)
-            if result != too_high:
-                mistakes = mistakes + 1
-        print("{}/{} mistakes".format(mistakes, reps))
+    print("{}/{} mistakes".format(mistakes, reps))
     
