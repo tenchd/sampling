ba1f0b4396c9181f6a8a053bc26bc8962d83bde2
tenchd <dmt213@gmail.com>
2019-09-08 21:44:43 -0400

    not sure what i changed

--- f_0sketcher.py
+++ f_0sketcher.py
@@ -17,6 +17,7 @@ class HashFn():
         seed = getrandbits(32)
         self.fn = xxhash.xxh32(seed = seed)
         self.total = 0
+        self.bins = []
     
     def hasher(self, num):
         """give it a number (index for vector), it returns a random integer"""
@@ -31,6 +32,7 @@ class HashFn():
         hashed = self.hasher(index)
         if hashed <= 4294967296/T:
             self.total = self.total + value
+            self.bins.append(index)
             return True
         return False
 
@@ -42,23 +44,35 @@ def T_checker(T, stream, eps, delta, k_wt=1, display=False):
         for h in hashes:
             h.decider(index, element, T)
     nonzeroz = len(list(filter(lambda h: h.total != 0, hashes)))
+    threshold = k/(math.e)
     if display:
-        print(k/math.e)
+        print(threshold)
         print(nonzeroz)
-        if nonzeroz < k/math.e:
+        if nonzeroz < threshold:
             print("F_0 < (1-{})T = {} bro. You guessed too high".format(eps, (1-eps)*T))
         else:
             print("F_0 > (1+{})T  = {} superchief.  You guessed too low".format(eps, (1+eps)*T))
-    if nonzeroz < k/math.e:
+        #print([h.total for h in list(filter(lambda h: h.total != 0, hashes))])
+        #hash_lengths = 0
+        #all_bins = []
+        #for h in hashes:
+            #hash_lengths = hash_lengths + len(h.bins)
+            #all_bins.extend(h.bins)
+        #for i in range(100):
+            #print(i, all_bins.count(i))
+    if nonzeroz < threshold:
         return True
     else:
         return False
 
 if __name__ == '__main__':
+    #stream = np.identity(10, dtype=int).reshape(100,)
+    #T_checker(10, stream, .2, .01, display=True)
     F_0 = 100
     stream = np.identity((F_0), dtype=int).reshape(F_0**2,)
     T = 150
-    ks = [1, 1/2, 1/5, 1/10, 1/20]
+    #ks = [1, 1/2, 1/5, 1/10, 1/20]
+    ks = [1]
     if T > F_0:
         too_high = True
     else:
@@ -67,10 +81,10 @@ if __name__ == '__main__':
     for k in ks:
         mistakes = 0
         print("testing F_0 = {}, T = {}, k factor = {}".format(F_0, T, k))
-        reps = 100
+        reps = 1
         for i in range(reps):
             print(".", end = '')
-            result = T_checker(T, stream, .2, .01, k_wt=k, display=False)
+            result = T_checker(T, stream, .02, .01, k_wt=k, display=True)
             if result != too_high:
                 mistakes = mistakes + 1
         print("{}/{} mistakes".format(mistakes, reps))
