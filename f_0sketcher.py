bc5433fd8ea5440f05eb70f78923d03992d75548
tenchd <dmt213@gmail.com>
2019-09-09 14:55:48 -0400

    clean up code

--- f_0sketcher.py
+++ f_0sketcher.py
@@ -19,7 +19,6 @@ class HashFn():
         seed = getrandbits(32)
         self.fn = xxhash.xxh32(seed = seed)
         self.total = 0
-        #self.bins = set()
     
     def hasher(self, num):
         """Input: an integer (index for vector).  Output: a random integer."""
@@ -37,11 +36,13 @@ class HashFn():
         #2^32-1 = 4294967295 is max output value from hash function
         if hashed <= 4294967295/T: 
             self.total = self.total + value
-            #self.bins.add(index)
             return True
         return False
 
 def calc_k(eps, delta):
+    """Input: epsilon and delta error terms (floats).  Output: k, minimum # of
+    random sets required to guarantee multiplicative error factor eps with
+    probability of failure no more than delta. Via Chernoff bound argument."""
     term1 = -1*np.log(delta)
     term2 = eps/np.power(eps+math.e/3, 2)
     term3 = (1/math.e - eps/3)
@@ -49,7 +50,7 @@ def calc_k(eps, delta):
     result = term1*frac
     return result
 
-def T_checker(T, stream, eps, delta, real_k = True, display=False):
+def T_checker(T, stream, eps, delta, real_k = False, display=False):
     if real_k:
         k = int(calc_k(eps, delta))
     else:
@@ -59,36 +60,28 @@ def T_checker(T, stream, eps, delta, real_k = True, display=False):
     for index, element in enumerate(stream):
         for h in hashes:
             h.decider(index, element, T)
-    nonzeroz = len(list(filter(lambda h: h.total != 0, hashes)))
-    threshold = k- k/(math.e)
+    zeroz = len(list(filter(lambda h: h.total == 0, hashes)))
+    threshold = k/(math.e)
     if display:
         print("k is {}".format(k))
         print("threshold is {}".format(threshold))
-        print("# of nonzeros is {}".format(nonzeroz))
-        print("# of zeros is {}".format(k-nonzeroz))
+        print("# of zeros is {}".format(zeroz))
         
-        if nonzeroz < threshold:
+        if zeroz > threshold:
             print("F_0 < (1-{})T = {} bro. You guessed too high".format(eps, (1-eps)*T))
         else:
             print("F_0 > (1+{})T  = {} superchief.  You guessed too low".format(eps, (1+eps)*T))
-        #print([h.total for h in list(filter(lambda h: h.total != 0, hashes))])
-        #print([h.total for h in hashes])
-        #hash_lengths = 0
-        #all_bins = []
-        #for h in hashes:
-            #hash_lengths = hash_lengths + len(h.bins)
-            #all_bins.extend(h.bins)
-            #print(h.bins, h.total)
-        #for i in range(100):
-            #print(i, all_bins.count(i))
-    if nonzeroz < threshold:
+    if zeroz > threshold:
         return True
     else:
         return False
 
+
 if __name__ == '__main__':
-    #stream = np.identity(10, dtype=int).reshape(100,)
-    #T_checker(12, stream, .2, .01, k_wt=1, display=True)
+# =============================================================================
+#     stream = np.identity(10, dtype=int).reshape(100,)
+#     T_checker(12, stream, .2, .01, k_wt=1, display=True)
+# =============================================================================
     F_0 = 10
     stream = np.identity((F_0), dtype=int).reshape(F_0**2,)
     T = 12
@@ -96,12 +89,12 @@ if __name__ == '__main__':
         too_high = True
     else:
         too_high = False
-    real_k = False
+    real_k = True
     mistakes = 0
     print("testing F_0 = {}, T = {}, real k? {}".format(F_0, T, real_k))
     reps = 100
     for i in range(reps):
-        result = T_checker(T, stream, .2, .01, display=False)
+        result = T_checker(T, stream, .2, .01, real_k=real_k, display=False)
         if result != too_high:
             mistakes = mistakes + 1
             print("!", end = '')
