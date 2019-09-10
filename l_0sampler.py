34b876a21f92efe25fe016214ee523babe6af746
tenchd <dmt213@gmail.com>
2019-09-10 18:35:00 -0400

    working prototype except for prime generator

--- l_0sampler.py
+++ l_0sampler.py
@@ -8,9 +8,11 @@ Created on Wed Jul  3 00:19:46 2019
 
 import xxhash
 import numpy as np
-from random import getrandbits, randint
+from random import getrandbits, randint, choice
 import math
 import struct
+import sample_streams as strm
+import itertools
 
 class RandomIndexSubset():
     """Represents a random subset of [n] where each element is included 
@@ -63,8 +65,8 @@ class RandomIndexSubset():
         except:
             print('b is 0 so it failed.')
             return False
-        c_check = (self.b*pow(self.r, int(quotient), self.p))
-        if quotient == int(quotient) and c_check == self.c:
+        self.c_check = (self.b*pow(self.r, int(quotient), self.p))
+        if quotient == int(quotient) and self.c_check == self.c:
             self.queryable = True
             print('passed!')
             return True
@@ -87,18 +89,78 @@ class RandomIndexSubset():
         if self.queryable:
             index = int(self.a/self.b)
             value = self.b
+            print('a = {} b = {} c = {} a/b = {} r = {} c_check = {}, p = {}'.format(self.a, self.b, self.c, self.a/self.b, self.r, self.c_check, self.p))
             return index, value
         else:
             raise Exception('you tried to sample from subset {} when it hadn\'t been checked successfully'.format(self.i))
+    
+
+
+class l_0_sketch():
+    """Warning: only ever sample _ONCE_ from an l_0 sketch.  Taking more samples 
+    (ESPECIALLY if you try to subtract the first value you sampled, and then 
+    sample again in hopes of getting a second distinct sample) WILL NOT yield
+    the desired statistical properties and may actually output nonsense."""
+    def __init__(self, n):
+        self.n = n
+        self.p = self.choose_p()
+        #how many times we need to repeat the random processes for high prob of success.  maybe excessive on outer list comp?
+        reps = int(math.log2(n))+1
+        self.subsets = [[RandomIndexSubset(i, self.p) for i in range(reps)] for j in range(reps)]
+# =============================================================================
+#         for i in self.subsets:
+#             print([j.hasher(50) for j in i])
+# =============================================================================
+        self.sampled = False
+        
+    def choose_p(self):
+        """p must be prime and also poly(n). If someone can write efficient
+        code that finds a prime close to but larger than target, l_0_sketch 
+        is done"""
+        target = pow(self.n,2)
+        return target
+    
+    def update(self, index, value):
+        for mini_sketch in self.subsets:
+            for s in mini_sketch:
+                s.update(index, value)
+    
+    def process_stream(self, stream):
+        for index, value in stream:
+            self.update(index, value)
+    
+    def check_mini_sketch(self, mini_sketch):
+        passes = []
+        for subset in mini_sketch:
+            if subset.check():
+                passes.append(subset.i)
+        if passes is not []:
+            return passes
+        return False
+        
+        
+    def l_0_sample(self):
+        if self.sampled:
+            raise Exception('You already sampled from this sketch. Sampling again is potentially fatal to you, the user.  Didn\'t you read the docstring?')
+        self.sampled = True
+        for mini_sketch in self.subsets:
+            passed = self.check_mini_sketch(mini_sketch)
+            if passed:
+                #choose a passing subset in the minisketch at random and get its index/value pair
+                index, value = mini_sketch[choice(passed)].sample()
+                return index, value
+        return False
+            
+        
+        
+    
+    
+    
 
 if __name__ == '__main__':
-    n = 100
-    p = pow(n, 3)
-    s = RandomIndexSubset(6, p)
-    stream = range(50)
-    for index, value in enumerate(stream):
-        s.update(index, value)
-    #s.update(50, 1)
-    s.check()
-    print(s.sample())
+    n = 127
+    l = l_0_sketch(n)
+    stream = itertools.chain(iter(strm.SampleStream(n)),iter(strm.SampleStream2(n)))
+    l.process_stream(stream)
+    print(l.l_0_sample())
     
