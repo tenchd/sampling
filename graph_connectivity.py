4afb715f775a1c040231a194eb50a61edf823bee
tenchd <dmt213@gmail.com>
2019-09-13 13:52:31 -0400

    full working graph connectivity prototype

--- graph_connectivity.py
+++ graph_connectivity.py
@@ -15,7 +15,7 @@ def ite(n, index):
     m = n*(n-1)/2
     i = n-int(0.5*(1 + math.sqrt(8*(m-index-1)+1)))-1
     j = index + i - int(m-(n-i)*(n-i-1)/2)+1
-    print(i,j)
+    #print(i,j)
     return i,j
     
 class Edge():
@@ -61,7 +61,7 @@ class Supernode():
             sample = sketch.l_0_sample(channel=node)
         else:
             terms = tuple((i,1) for i in self.nodes)
-            print('querying {}'.format(terms))
+            #print('querying {}'.format(terms))
             sample = sketch.l_0_sample_linear(terms)
         if sample==False:
             return False
@@ -77,31 +77,41 @@ class Supernode_Set():
         self.supernodes = {i:Supernode(i) for i in range(n)}
     
     def boruvka_round(self, sketch):
-        """Returns False if either no edges between connected components were 
+        """Returns True if either no edges between connected components were 
         discovered, or if all nodes are part of the same connected component.  
-        True otherwise."""
-        edges = []
+        False otherwise."""
+        edges = set()
+        change = False
         #sample an edge incident to each connected component
-        for node, cc in self.supernodes.items():
+        #need to loop over only unique ccs
+        for cc in set(cc for cc in self.supernodes.values()):
+            print('looking for an edge out of {}'.format(cc.nodes))
             result = cc.sample(sketch)
             if result != False:
+                change = True
                 i,j = result
                 e = Edge(i,j)
-                edges.append(e)
+                edges.add(e)
+        if not change:
+            return True
         #merge connected components based on discovered edges
         for e in edges:
             i,j = e.i, e.j
+            #print('combining {} and {}'.format(self.supernodes[i].nodes, self.supernodes[j].nodes))
             a = self.supernodes[i]
             b = self.supernodes[j]
             c = Supernode(parts=[a,b])
             #overwrite previous supernodes a and b
-            #BUG HERE SOMEHOW
             for node in c.nodes:
-                self.supernodes[i] = c
+                self.supernodes[node] = c
+        if len(self.supernodes[0].nodes)==n:
+            return True
+        return False
                 
     def display(self):
+        print('displaying supernode set')
         for n, cc in self.supernodes.items():
-            print(cc.nodes)
+            print(n, cc.nodes)
 
 def edge_sketcher(m, edge, sketch):
     """Translates an edge update into the signed vertex/edge vector form and
@@ -119,11 +129,10 @@ if __name__ == '__main__':
     m = int(n*(n-1)/2)
     rounds = int(math.log2(n))+1
     sketches = [sktch.l_0_sketch(m, channels=10) for r in range(rounds)]
-    print(sketches)
     
     edges = [Edge(0,i,insert=True) for i in range(1,10)]
     edges.extend([Edge(0,j,insert=False) for j in range(2,10,2)])
-    edges.append(Edge(1,5,insert=True))
+    edges.extend([Edge(3,k,insert=True) for k in range(4,10,2)])
     
     for e in edges:
         for sketch in sketches:
@@ -132,7 +141,17 @@ if __name__ == '__main__':
     
     s = Supernode_Set(n)
     s.display()
-    s.boruvka_round(sketches[0])
-    s.display()
-    s.boruvka_round(sketches[1])
-    s.display()
+    done = False
+    b_round = 0
+# =============================================================================
+#     s.display()
+#     s.boruvka_round(sketches[0])
+#     s.display()
+#     s.boruvka_round(sketches[1])
+#     s.display()
+# =============================================================================
+    while not done:
+        done = s.boruvka_round(sketches[b_round])
+        print('Are we done yet? {}'.format(done))
+        b_round += 1
+        s.display()
